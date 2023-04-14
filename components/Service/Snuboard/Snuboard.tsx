8c6ad6d4e9b557eb1a7cc848b68dec4c02edc25d
isuk <ihyungsuk@snu.ac.kr>
2023-04-14 18:14:06 +0900

    Edit : Snuboard.tsx Introduction 작업, Snuboard.module.scss 기본 틀만 작업

--- components/Service/Snuboard/Snuboard.tsx
+++ components/Service/Snuboard/Snuboard.tsx
@@ -18,8 +18,31 @@ function Snuboard() {
     <section className={cx("container", scrollState)} ref={ref}>
       <div className={cx("background")}></div>
       <div className={cx("introduction")}>
-        {/* <IntroductionHead />
-        <IntroductionCarousel /> */}
+        <IntroductionHead
+          logo={"/static/images/logo/snuboard_logo.svg"}
+          title={"스누보드"}
+          des={[
+            "SNUTT는 서울대학교 시간표 어플입니다.",
+            "SNUTT는 서울대학교 대표 시간표 어플입니다.",
+            "SNUTT는 서울대학교 시간표 어플입니다.",
+          ]}
+          textColor="#ffffff"
+          // TODO: 변수명 link => links 변경 제의
+          link={[
+            "https://www.wafflestudio.com/",
+            "https://www.wafflestudio.com/",
+            "https://www.wafflestudio.com/",
+          ]}
+        />
+        <IntroductionCarousel
+          carouselImages={[
+            "/static/images/dummy/coMo.png",
+            "/static/images/dummy/seminar.png",
+            "/static/images/dummy/Untitled.png",
+            "/static/images/dummy/seminar.png",
+            "/static/images/dummy/coMo.png",
+          ]}
+        />
       </div>
     </section>
   );
