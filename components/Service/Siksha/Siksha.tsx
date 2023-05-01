09647b903a4fdd93f47c768e9d8c2557dbcdd42e
minkyu <whizkyu@snu.ac.kr>
2023-05-01 16:16:41 +0900

    캐러셀 추가

--- components/Service/Siksha/Siksha.tsx
+++ components/Service/Siksha/Siksha.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames/bind";
 import useWaffleScroll from "../../../library/waffleScroll";
+import IntroductionCarousel from "../common/IntroductionCarousel/IntroductionCarousel";
+import IntroductionHead from "../common/IntroductionHead/IntroductionHead";
 import styles from "./Siksha.module.scss";
 
 const cx = classNames.bind(styles);
@@ -12,29 +14,59 @@ function Siksha() {
     { available: false },
   );
   return (
-    <section className={cx("container", scrollState)} ref={ref}>
-      <div className={cx("background")}>
-        <div className={cx("leftContainer")}>
-          <img
-            className={cx("spoon")}
-            src={"/static/images/illustration/spoon.svg"}
-          />
-        </div>
-        <div className={cx("centerContainer")}></div>
-        <div className={cx("rightContainer")}>
-          <div className={cx("chopsticks")}>
+    <>
+      <section className={cx("container", scrollState)} ref={ref}>
+        <div className={cx("background")}>
+          <div className={cx("leftContainer")}>
             <img
-              className={cx("leftChopstick")}
-              src={"/static/images/illustration/chopstick.svg"}
-            />
-            <img
-              className={cx("rightChopstick")}
-              src={"/static/images/illustration/chopstick.svg"}
+              className={cx("spoon")}
+              src={"/static/images/illustration/spoon.svg"}
             />
           </div>
+          <div className={cx("centerContainer")}>
+            <div className={cx("introduction")}>
+              <IntroductionHead
+                logo={"/static/images/logo/snutt_logo.svg"}
+                title={"SNUTT"}
+                des={[
+                  "SNUTT는 서울대학교 시간표 어플입니다.",
+                  "SNUTT는 서울대학교 대표 시간표 어플입니다.",
+                  "SNUTT는 서울대학교 시간표 어플입니다.",
+                ]}
+                textColor="black"
+                link={[
+                  "https://www.wafflestudio.com/",
+                  "https://www.wafflestudio.com/",
+                  "https://www.wafflestudio.com/",
+                ]}
+              />
+              <IntroductionCarousel
+                carouselImages={[
+                  "/static/images/dummy/coMo.png",
+                  "/static/images/dummy/seminar.png",
+                  "/static/images/dummy/Untitled.png",
+                  "/static/images/dummy/seminar.png",
+                  "/static/images/dummy/coMo.png",
+                  "/static/images/dummy/mobile.png",
+                ]}
+              />
+            </div>
+          </div>
+          <div className={cx("rightContainer")}>
+            <div className={cx("chopsticks")}>
+              <img
+                className={cx("leftChopstick")}
+                src={"/static/images/illustration/chopstick.svg"}
+              />
+              <img
+                className={cx("rightChopstick")}
+                src={"/static/images/illustration/chopstick.svg"}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
