f5b13544e45ea7872165a8f56edbfb07df592ce4
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-14 03:35:18 +0900

    Feat: Header 컴포넌트

--- pages/newHome/index.tsx
+++ pages/newHome/index.tsx
@@ -1,5 +1,13 @@
+import classNames from "classnames/bind";
+import Header from "../../components/common/Header/Header";
 import styles from "./Home.module.scss";
 
+const cx = classNames.bind(styles);
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Header />
+    </main>
+  );
 }
