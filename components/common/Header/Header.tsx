ecad2d840640620a84963ecc779eca1531f9b6e6
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-14 00:59:38 +0900

    Feat: 헤더와 스크롤바 컴포넌트 기본 세팅

---
+++ components/common/Header/Header.tsx
@@ -0,0 +1,5 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return <header></header>;
+}
---
+++ components/common/Scroll/ScrollNavigator.tsx
@@ -0,0 +1,5 @@
+import styles from "./ScrollNavigator.module.scss";
+
+export default function ScrollNavigator() {
+  return <nav></nav>;
+}
