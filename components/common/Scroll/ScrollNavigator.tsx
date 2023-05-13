39878398de2e3933927e22d1d839ded725a86949
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-14 04:00:37 +0900

    Feat: 스크롤 내비게이터 추가

--- components/common/Scroll/ScrollNavigator.tsx
+++ components/common/Scroll/ScrollNavigator.tsx
@@ -1,5 +1,37 @@
+import classNames from "classnames/bind";
 import styles from "./ScrollNavigator.module.scss";
+const cx = classNames.bind(styles);
+
+type ScrollItemProps = {
+  name: string;
+  anchorId: string;
+};
+
+const homeScrolls: ScrollItemProps[] = [
+  { name: "MAIN", anchorId: "main" },
+  { name: "ABOUT US", anchorId: "about" },
+  { name: "OUR SERVICES", anchorId: "services" },
+  { name: "MEMBERS", anchorId: "members" },
+  { name: "ACTIVITY", anchorId: "activity" },
+];
+
+function ScrollItem({ name, anchorId }: ScrollItemProps) {
+  return (
+    <li className={cx("item")} onClick={() => {}}>
+      <div className={cx("label")}>{name}</div>
+      <div className={cx("dot")} />
+    </li>
+  );
+}
 
 export default function ScrollNavigator() {
-  return <nav></nav>;
+  return (
+    <nav className={cx("ScrollNavigator")}>
+      <ul className={cx("list")}>
+        {homeScrolls.map(({ name, anchorId }) => (
+          <ScrollItem key={anchorId} name={name} anchorId={anchorId} />
+        ))}
+      </ul>
+    </nav>
+  );
 }
--- pages/newHome/index.tsx
+++ pages/newHome/index.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames/bind";
 import Header from "../../components/common/Header/Header";
+import ScrollNavigator from "../../components/common/Scroll/ScrollNavigator";
 import styles from "./Home.module.scss";
 
 const cx = classNames.bind(styles);
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main>
       <Header />
+      <ScrollNavigator />
     </main>
   );
 }
