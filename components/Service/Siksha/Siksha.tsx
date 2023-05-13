c56433a2c6de8f1d492a3621984758bff3902e5b
minkyu <whizkyu@snu.ac.kr>
2023-05-13 15:08:20 +0900

    link -> links 오타 수정

--- components/Service/Siksha/Siksha.tsx
+++ components/Service/Siksha/Siksha.tsx
@@ -34,7 +34,7 @@ function Siksha() {
                   "SNUTT는 서울대학교 시간표 어플입니다.",
                 ]}
                 textColor="black"
-                link={[
+                links={[
                   "https://www.wafflestudio.com/",
                   "https://www.wafflestudio.com/",
                   "https://www.wafflestudio.com/",
