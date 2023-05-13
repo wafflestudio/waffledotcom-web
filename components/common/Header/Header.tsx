60a09011ff5f7a910e64d8bac1ba742faffbd045
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-14 03:35:18 +0900

    Feat: Header 컴포넌트

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,5 +1,41 @@
+import classNames from "classnames/bind";
+import { useRouter } from "next/router";
 import styles from "./Header.module.scss";
+const cx = classNames.bind(styles);
 
+type TabItemProps = {
+  name: string;
+  pathname: string;
+};
+
+function TabItem({ name, pathname }: TabItemProps) {
+  const router = useRouter();
+  return (
+    <li
+      className={cx("tabItem", {
+        selected: router.pathname === pathname,
+      })}
+      onClick={() => {
+        router.push(pathname);
+      }}
+    >
+      {name}
+    </li>
+  );
+}
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className={cx("header")}>
+      {/*TODO: 개발/배포 주소 환경변수로 뺄까? */}
+      <a className={cx("logo")} href="http://localhost:3000/newHome/">
+        <img src="/static/images/logo/waffle_logo_title.png" />
+      </a>
+      <ul className={cx("tabNavigator")}>
+        {/*TODO: 라우팅 확정되면 newHome 바꾸기 */}
+        <TabItem name="소개" pathname="/newHome" />
+        <TabItem name="서비스" pathname="/service" />
+        <TabItem name="멤버" pathname="/member" />
+      </ul>
+    </header>
+  );
 }
