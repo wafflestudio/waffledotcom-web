5466deff8e4959fc4f1888d641768b772da5d28e
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-17 17:03:18 +0900

    Temp: About 섹션 작업 중

---
+++ components/newHome/About/About.tsx
@@ -0,0 +1,38 @@
+import classNames from "classnames/bind";
+import { useNavigatorScroll } from "../../Home/scroll";
+import styles from "./About.module.scss";
+
+const cx = classNames.bind(styles);
+
+export default function About() {
+  const { state, targetRef } = useNavigatorScroll({
+    callback: ({ progress, setState }) => {
+      if (progress >= 1) {
+        setState({ currentSection: "about" });
+      }
+    },
+    anchorId: "about",
+  });
+  return (
+    <section
+      className={cx("container", { off: state.currentSection !== "about" })}
+      ref={targetRef}
+    >
+      <div className={cx("background")} />
+      <div className={cx("content")}>
+        <div className={cx("name")}>
+          <h1>와플</h1>
+          <h1>스튜디오</h1>
+        </div>
+        <div className={cx("description")}>
+          <h3>와플스튜디오는 어떤 동아리인가요?</h3>
+          <p>
+            와플스튜디오는 서울대학교 컴퓨터공학부에서 탄생한 웹/앱 개발
+            동아리에요. 개발자들의 커뮤니티 형성을 도모하고, 개발에 대해 서로
+            이야기하고 경험하며 성장하는 것을 추구해요.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
