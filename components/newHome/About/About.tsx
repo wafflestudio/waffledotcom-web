ba1bf89c026e626e9d3083a702644cb846809d05
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-17 17:37:10 +0900

    Feat: About 마무리

--- components/newHome/About/About.tsx
+++ components/newHome/About/About.tsx
@@ -19,18 +19,31 @@ export default function About() {
       ref={targetRef}
     >
       <div className={cx("background")} />
-      <div className={cx("content")}>
-        <div className={cx("name")}>
-          <h1>와플</h1>
-          <h1>스튜디오</h1>
-        </div>
-        <div className={cx("description")}>
-          <h3>와플스튜디오는 어떤 동아리인가요?</h3>
-          <p>
-            와플스튜디오는 서울대학교 컴퓨터공학부에서 탄생한 웹/앱 개발
-            동아리에요. 개발자들의 커뮤니티 형성을 도모하고, 개발에 대해 서로
-            이야기하고 경험하며 성장하는 것을 추구해요.
-          </p>
+      <div className={cx("foreground")}>
+        <div className={cx("content")}>
+          <div className={cx("name")}>
+            <h1>와플</h1>
+            <h1>스튜디오</h1>
+            <img src={"/static/images/logo/pupuri_logo.png"} />
+          </div>
+          <div className={cx("description")}>
+            <div className={cx("question")}>
+              <h3>와플스튜디오는 어떤 동아리인가요?</h3>
+              <p>
+                와플스튜디오는 서울대학교 컴퓨터공학부에서 탄생한 웹/앱 개발
+                동아리에요. 개발자들의 커뮤니티 형성을 도모하고, 개발에 대해
+                서로 이야기하고 경험하며 성장하는 것을 추구해요.
+              </p>
+            </div>
+            <div className={cx("question")}>
+              <h3>와플스튜디오는 어떤 동아리인가요?</h3>
+              <p>
+                와플스튜디오는 서울대학교 컴퓨터공학부에서 탄생한 웹/앱 개발
+                동아리에요. 개발자들의 커뮤니티 형성을 도모하고, 개발에 대해
+                서로 이야기하고 경험하며 성장하는 것을 추구해요.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
