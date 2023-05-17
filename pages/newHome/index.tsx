340bc5e1e368793a3f4fd9749c6e119c355fcf64
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-17 16:47:27 +0900

    Feat: 메인 섹션 추가

--- pages/newHome/index.tsx
+++ pages/newHome/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import Header from "../../components/common/Header/Header";
 import ScrollNavigator from "../../components/common/Scroll/ScrollNavigator";
 import { useNavigatorScroll } from "../../components/Home/scroll";
+import Main from "../../components/Home/Main/Main";
 import styles from "./Home.module.scss";
 
 const cx = classNames.bind(styles);
@@ -89,7 +90,7 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <TestMain />
+      <Main />
       <TestAbout />
       <TestServices />
       <TestMembers />
