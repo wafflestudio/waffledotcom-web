642324331df956d6db41ec33c725f41697f25054
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-22 04:35:19 +0900

    Feat: About 더보기

--- components/newHome/About/About.tsx
+++ components/newHome/About/About.tsx
@@ -1,10 +1,12 @@
 import classNames from "classnames/bind";
+import { useState } from "react";
 import { useNavigatorScroll } from "../../Home/scroll";
 import styles from "./About.module.scss";
 
 const cx = classNames.bind(styles);
 
 export default function About() {
+  const [more, setMore] = useState(false);
   const { state, targetRef } = useNavigatorScroll({
     callback: ({ progress, setState }) => {
       if (progress >= 1 && progress < 3) {
@@ -36,12 +38,33 @@ export default function About() {
               </p>
             </div>
             <div className={cx("question")}>
-              <h3>와플스튜디오는 어떤 동아리인가요?</h3>
+              <h3>와플스튜디오는 어떤 활동이 진행되나요?</h3>
               <p>
-                와플스튜디오는 서울대학교 컴퓨터공학부에서 탄생한 웹/앱 개발
-                동아리에요. 개발자들의 커뮤니티 형성을 도모하고, 개발에 대해
-                서로 이야기하고 경험하며 성장하는 것을 추구해요.
+                와플스튜디오에서는 이런 성장을 위해 다양한 활동이 진행돼요.
+                대표적으로 프로젝트와 스터디가 있는데, 스터디 같은 경우 개발과
+                관련하여 원하는 주제가 있다면 자유롭게 열 수 있어요. 저희의
+                근본적인 목적은 “개발 공부”랍니다!{" "}
+                {!more && (
+                  <span>
+                    ...{" "}
+                    <span
+                      className={cx("underline")}
+                      onClick={() => setMore(true)}
+                    >
+                      더보기
+                    </span>
+                  </span>
+                )}
               </p>
+              {more && (
+                <p>
+                  <br />
+                  또한 개발자간의 커뮤니티를 도모하기 위해 “코딩 모임”이라는
+                  모임이 매월 자유롭게 열려요. 코딩을 좋아하는 사람들끼리 모여서
+                  각자 할 일을 하게 돼요. 그러면서 코딩이나 개발과 관련된
+                  이야기도 나누고, 친목을 다질 수 있어요!
+                </p>
+              )}
             </div>
           </div>
         </div>
