12738b8f95aa464fdb0ca8003c0b631526819da6
designDefined <designdefined.by@gmail.com>
2023-09-03 03:50:23 +0900

    Fix: build error of old v2 home

--- components/deprecated/Home/Billboard/dummy.ts
+++ components/deprecated/Home/Billboard/dummy.ts
@@ -1,8 +1,8 @@
 import { StaticImageData } from "next/legacy/image";
-import image1 from "../../../public/static/images/dummy/untitled2.png";
-import image2 from "../../../public/static/images/dummy/Untitled.png";
-import image3 from "../../../public/static/images/dummy/Rectangle 80.png";
-import image4 from "../../../public/static/images/dummy/coMo.png";
+import image1 from "../../../../public/static/images/dummy/untitled2.png";
+import image2 from "../../../../public/static/images/dummy/Untitled.png";
+import image3 from "../../../../public/static/images/dummy/Rectangle 80.png";
+import image4 from "../../../../public/static/images/dummy/coMo.png";
 export type IDummyBanner = {
   title: string;
   backgroundColor: string;
