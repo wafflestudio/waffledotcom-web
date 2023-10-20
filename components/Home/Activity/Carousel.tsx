479071c994c18872fe80f576b918990d57b66954
MINKYU LEE <whizkyu@snu.ac.kr>
2023-10-20 21:05:52 +0900

    Feat: 캐러셀에 scroll-snap 적용 및 코드 개선 (#52)
    * fix: PC 활동 이미지 그림자 / 둥근 모서리
    
    * feat: scroll snap 활용한 캐러셀
    
    * 캐러셀 인디케이터 애니메이션 적용
    
    * 사파리 캐러셀 버그 수정

--- components/Home/Activity/Carousel.tsx
+++ components/Home/Activity/Carousel.tsx
@@ -1,55 +1,65 @@
 import classNames from "classnames/bind";
+import { useCallback, useMemo } from "react";
 import styles from "./Carousel.module.scss";
-import { Activity } from "./Activity";
+import { ActivityData } from "./ActivityData";
 
 const cx = classNames.bind(styles);
 
 type CarouselProps = {
-  activities: Activity[];
+  activities: ActivityData[];
   selectedId: number;
+  setSelectedId: (_: number | ((_: number) => number)) => void;
 };
 
-export default function Carousel({ activities, selectedId }: CarouselProps) {
+export default function Carousel({
+  activities,
+  selectedId,
+  setSelectedId,
+}: CarouselProps) {
+  const carouselScrollHandler = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      const stride = window.innerWidth * 0.85;
+      setSelectedId(Math.round(e.currentTarget.scrollLeft / stride));
+    },
+    [],
+  );
+
   return (
     <>
       <div className={cx("carousel")}>
-        <div className={cx("carousel-frame")}>
+        <div className={cx("carouselFrame")}>
           <div
-            className={cx("carousel-items-container", `state-${selectedId}`)}
+            className={cx("carouselItemsContainer")}
+            onScroll={carouselScrollHandler}
           >
-            {activities.map((activity, _) => (
-              <CarouselItem key={_} activity={activity} />
+            {activities.map((activity, id) => (
+              <CarouselItem key={id} activity={activity} />
             ))}
           </div>
         </div>
       </div>
 
-      <ul className={cx("dots")}>
-        {[0, 1, 2, 3, 4].map((id) =>
-          id === selectedId ? (
-            <li key={id} className={cx("rectangle")}>
-              <img src="/static/images/activity/rectangle_orange.svg" alt="" />
-            </li>
-          ) : (
-            <li key={id} className={cx("dot")}>
-              <img src="/static/images/activity/dot_gray.svg" alt="" />
-            </li>
-          ),
-        )}
+      <ul className={cx("carouselIndicator")}>
+        {[0, 1, 2, 3, 4].map((id) => (
+          <div
+            key={id}
+            className={cx("dot", id === selectedId ? "selected" : "")}
+          />
+        ))}
       </ul>
     </>
   );
 }
 
 type CarouselItemProps = {
-  activity: Activity;
+  activity: ActivityData;
 };
 
 function CarouselItem({ activity }: CarouselItemProps) {
   return (
-    <div className={cx("carousel-item")}>
+    <div className={cx("carouselItem")}>
       <div className={cx("imageContainer")}>
-        <img src={activity.image} alt={activity.altImg} />
+        <img src={activity.image} alt={activity.altText} />
       </div>
       <div className={cx("textContainer")}>
         <h1 className={cx("activityTitle")}>{activity.head}</h1>
