fd72e659bb0485198f4ecfcc1f330d3a57013763
designDefined <designdefined.by@gmail.com>
2025-01-07 04:18:40 +0900

    후원자 추가

--- components/Home/Support/Support.tsx
+++ components/Home/Support/Support.tsx
@@ -37,6 +37,15 @@ export default function Support() {
             <div className={styles.sponsor}>박면규</div>
             <div className={styles.sponsor}>한재화</div>
             <div className={styles.sponsor}>이승찬</div>
+            <div className={styles.sponsor}>김지호</div>
+          </div>
+
+          <div className={styles.sponsors}>
+            <div className={styles.sponsor}>이인용</div>
+            <div className={styles.sponsor}>김지환</div>
+            <div className={styles.sponsor}>김정은</div>
+            <div className={styles.sponsor}>김광래</div>
+            <div className={styles.sponsor}>이종민</div>
           </div>
         </div>
         <div className={styles.bottom}>
@@ -85,7 +94,7 @@ export default function Support() {
                 </div>
                 <div className={styles.row}>
                   <span className={cx("label")}>와플 동아리장</span>
-                  snjlee58@snu.ac.kr
+                  eastshine@snu.ac.kr
                 </div>
               </div>
             </div>
